3e3b09ce7617446a818e1adbd96d6abc3b2f4958
Florian N'gbala <45558751+florianNgbala@users.noreply.github.com>
2021-01-23 15:06:11 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        tonchienettoi.github.io yyy
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <p>Test</p>      
   </div>
 </template>
 
