b12d6134cc6a01bc3d7f71d03f08e16ceda8acd4
Florian N'gbala <45558751+florianNgbala@users.noreply.github.com>
2021-01-23 15:13:18 +0100

    e

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/tonchienettoi/'
+    base: '/tonchienettoi.github.io/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
